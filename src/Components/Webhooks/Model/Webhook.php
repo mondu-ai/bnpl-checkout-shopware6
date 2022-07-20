9956fb4f78b3a03a9ac96b30fce36fe6c5684da1
harism2 <97665980+harism2@users.noreply.github.com>
2022-07-20 11:22:15 +0200

    Added webhook registration (#10)

---
+++ src/Components/Webhooks/Model/Webhook.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mondu\MonduPayment\Components\Webhooks\Model;
+
+class Webhook
+{
+    private string $topic;
+    private string $address;
+
+    public function __construct($topic)
+    {
+      $this->topic = $topic;
+      $this->address = $_SERVER['HTTP_ORIGIN'] . "/mondu/webhooks";
+    }
+
+    public function getTopic(): string
+    {
+      return $this->topic;
+    }
+
+    public function getAddress(): string
+    {
+      return $this->address;
+    }
+
+    public function getData()
+    {
+      return [
+        'topic' => $this->getTopic(),
+        'address' => $this->getAddress()
+      ];
+    }
+}
