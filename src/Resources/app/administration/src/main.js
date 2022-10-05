efa5cdedaaf37a010e99cfdf6094eeb65991ec3a
harism2 <97665980+harism2@users.noreply.github.com>
2022-10-05 16:03:14 +0200

    Added API validation button in plugin configuration (#20)
    * Added API validation button in plugin configuration
    
    * Fixed conflicts
    
    * Bumped plugin version

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,3 +1,5 @@
 import './module/sw-order';
-import './init/invoice-service.init'
-import './init/credit_note-service.init'
+import './init/invoice-service.init';
+import './init/credit_note-service.init';
+import './init/mondu-config-service.init';
+import './module/sw-mondu-test-api-button';
