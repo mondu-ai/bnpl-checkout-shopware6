70a31cee1619a31193ec2a880255b43d9c0d0dc1
Tigran <62335544+tikohov20@users.noreply.github.com>
2023-05-03 14:05:22 +0400

    Document controller fix for 6.4.14 + (#46)

--- src/Components/Order/Controller/DocumentController.php
+++ src/Components/Order/Controller/DocumentController.php
@@ -5,36 +5,22 @@ declare(strict_types=1);
 namespace Mondu\MonduPayment\Components\Order\Controller;
 
 use Mondu\MonduPayment\Components\Order\Util\DocumentUrlHelper;
-use Shopware\Core\Checkout\Document\DocumentService;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @RouteScope(scopes={"storefront"})
  */
 class DocumentController extends \Shopware\Core\Checkout\Document\Controller\DocumentController
 {
-    private DocumentUrlHelper $documentUrlHelper;
-
-    public function __construct(
-        DocumentService $documentService,
-        EntityRepositoryInterface $documentRepository,
-        DocumentUrlHelper $documentUrlHelper
-    ) {
-        parent::__construct($documentService, $documentRepository);
-        $this->documentUrlHelper = $documentUrlHelper;
-    }
-
     /**
      * @Route(name="mondu-payment.payment.document", path="/mondu/document/{documentId}/{deepLinkCode}/{token}")
      */
     public function downloadDocument(Request $request, string $documentId, string $deepLinkCode, Context $context): Response
     {
-        if ($this->documentUrlHelper->getToken() !== $request->attributes->get('token')) {
+        $documentUrlHelper = $this->container->get(DocumentUrlHelper::class);
+        if ($documentUrlHelper->getToken() !== $request->attributes->get('token')) {
             throw $this->createNotFoundException();
         }
 
