bfd1f4a1bc38f6f599a80ce71f74a7a1d20bcdb5
harism2 <97665980+harism2@users.noreply.github.com>
2023-06-22 12:35:58 +0200

    Updated payment methods to support hosted checkout (#53)

--- src/Components/PaymentMethod/PaymentHandler/MonduHandler.php
+++ src/Components/PaymentMethod/PaymentHandler/MonduHandler.php
@@ -1,58 +1,238 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Mondu\MonduPayment\Components\PaymentMethod\PaymentHandler;
 
 use Mondu\MonduPayment\Components\Order\Model\OrderDataEntity;
-use Mondu\MonduPayment\Components\MonduApi\Service\MonduClient;
-use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\SynchronousPaymentHandlerInterface;
-use Shopware\Core\Checkout\Payment\Cart\SyncPaymentTransactionStruct;
-use Shopware\Core\Checkout\Payment\Exception\SyncPaymentProcessException;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\AsynchronousPaymentHandlerInterface;
+use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
+use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
-use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
+use Mondu\MonduPayment\Components\MonduApi\Service\MonduClient;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Checkout\Cart\Price\Struct\CartPrice;
+use Mondu\MonduPayment\Components\PaymentMethod\Util\MethodHelper;
 
-class MonduHandler implements SynchronousPaymentHandlerInterface
+class MonduHandler implements AsynchronousPaymentHandlerInterface
 {
     private OrderTransactionStateHandler $transactionStateHandler;
     private MonduClient $monduClient;
-    private $orderDataRepository;
-    private $orderRepository;
+    private EntityRepository $productRepository;
+    private EntityRepository $orderDataRepository;
 
-    public function __construct(OrderTransactionStateHandler $transactionStateHandler, MonduClient $monduClient, $orderDataRepository, EntityRepository $repository)
+
+    public function __construct(OrderTransactionStateHandler $transactionStateHandler, MonduClient $monduClient, EntityRepository $productRepository, EntityRepository $orderDataRepository)
     {
-        $this->transactionStateHandler = $transactionStateHandler;
         $this->monduClient = $monduClient;
+        $this->productRepository = $productRepository;
+        $this->transactionStateHandler = $transactionStateHandler;
         $this->orderDataRepository = $orderDataRepository;
-        $this->orderRepository = $repository;
     }
 
-    public function pay(SyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): void
+    /**
+     * @throws AsyncPaymentProcessException
+     */
+    public function pay(AsyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): RedirectResponse
     {
-        $monduData = $dataBag->get('mondu_payment', new ParameterBag([]));
-
-        if ($monduData->count() === 0 || $monduData->has('order-id') === false || !$monduData->get('order-id')) {
-            throw new SyncPaymentProcessException($transaction->getOrderTransaction()->getId(), 'unknown error during payment');
+        try {
+            $redirectUrl = $this->createOrder($transaction, $salesChannelContext);
+        } catch (\Exception $e) {
+            throw new AsyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                'An error occurred during the communication with external payment gateway' . PHP_EOL . $e->getMessage()
+            );
         }
 
-        $order = $transaction->getOrder();
-        $monduOrder = $this->monduClient->setSalesChannelId($salesChannelContext->getSalesChannelId())->getMonduOrder($monduData->get('order-id'));
-        if (!$monduOrder) {
-            throw new SyncPaymentProcessException($transaction->getOrderTransaction()->getId(), 'unknown error during payment');
-        }
+        return new RedirectResponse($redirectUrl);
+    }
 
-        // Update external reference id on Mondu
+    /**
+     * @throws CustomerCanceledAsyncPaymentException
+     */
+    public function finalize(AsyncPaymentTransactionStruct $transaction, Request $request, SalesChannelContext $salesChannelContext): void
+    {
+        $transactionId = $transaction->getOrderTransaction()->getId();
+        $paymentState = $request->query->getAlpha('payment');
+        $context = $salesChannelContext->getContext();
 
-        $this->monduClient
-                ->setSalesChannelId($salesChannelContext->getSalesChannelId())
-                ->updateExternalInfo(
-                    $monduData->get('order-id'),
-                    ['external_reference_id' => $order->getOrderNumber()]
+        if ($paymentState === 'success') {
+            $paymentOrderUuid = $request->query->get('order_uuid');
+
+            $confirmResponseState = $this->monduClient->setSalesChannelId($salesChannelContext->getSalesChannelId())->confirmOrder($paymentOrderUuid);
+            
+            if ($confirmResponseState != 'confirmed') {
+                throw new CustomerCanceledAsyncPaymentException(
+                    $transactionId,
+                    'Order not confirmed.'
                 );
+            }
+            
+            $this->createLocalOrder($transaction, $paymentOrderUuid, $salesChannelContext);
 
+            $this->transactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context);
+        } else {
+            $this->transactionStateHandler->fail($transaction->getOrderTransaction()->getId(), $context);
+
+            throw new CustomerCanceledAsyncPaymentException(
+                $transactionId,
+                'Canceled/declined payment in Mondu Checkout.'
+            );
+        }
+    }
+
+    private function createOrder(AsyncPaymentTransactionStruct $transaction, SalesChannelContext $salesChannelContext): string
+    {
+        $orderData = $this->getOrderData($transaction, $salesChannelContext);
+
+        $monduOrder = $this->monduClient->setSalesChannelId($salesChannelContext->getSalesChannelId())->createOrder($orderData);
+
+        return $monduOrder['hosted_checkout_url'];
+    }
+
+    protected function getOrderData(AsyncPaymentTransactionStruct $transaction, SalesChannelContext $salesChannelContext)
+    {
+        $order = $transaction->getOrder();
+        $returnUrl = $transaction->getReturnUrl();
+        $orderTransaction = $transaction->getOrderTransaction();
+        $context = $salesChannelContext->getContext();
+
+        $lineItems = $this->getLineItems($order->getLineItems(), $context);
+        $shipping = $order->getShippingCosts()->getTotalPrice();
+
+        $discount = $this->getDiscount($order->getLineItems(), $context);
+
+        if ($context->getTaxState() === CartPrice::TAX_STATE_GROSS) {
+            $shipping = $order->getShippingCosts()->getTotalPrice() - $order->getShippingCosts()->getCalculatedTaxes()->getAmount();
+        } else {
+            $shipping = $order->getShippingCosts()->getTotalPrice();
+        }
+
+        $shippingAddress = $order->getDeliveries()->getShippingAddress()->first();
+
+        $paymentMethod = MethodHelper::shortNameToMonduName($orderTransaction->getPaymentMethod()->getShortName());
+
+        return [
+            'currency' => $order->getCurrency()->getIsoCode(),
+            'state_flow' => 'authorization_flow',
+            'payment_method' => $paymentMethod,
+            'success_url' => $returnUrl . '&payment=success',
+            'cancel_url' => $returnUrl . '&payment=cancelled',
+            'declined_url' => $returnUrl . '&payment=declined',
+            'external_reference_id' => $order->getOrderNumber(),
+            'gross_amount_cents' => round($order->getPrice()->getTotalPrice() * 100),
+            'buyer' => [
+                'email' => $order->getOrderCustomer()->getEmail(),
+                'first_name' => $order->getOrderCustomer()->getFirstname(),
+                'last_name' => $order->getOrderCustomer()-> getLastName(),
+                'company_name' => $order->getOrderCustomer()->getCompany(),
+                'phone' => $order->getBillingAddress()->getPhoneNumber(),
+                'address_line1' => $order->getBillingAddress()->getStreet(),
+                'zip_code' => $order->getBillingAddress()->getZipCode(),
+                'is_registered' => !$order->getOrderCustomer()->getCustomer()->getGuest()
+            ],
+            'billing_address' => [
+                'address_line1' => $order->getBillingAddress()->getStreet(),
+                'city' => $order->getBillingAddress()->getCity(),
+                'country_code' => $order->getBillingAddress()->getCountry()->getIso(),
+                'zip_code' => $order->getBillingAddress()->getZipCode(),
+            ],
+            'shipping_address' => [
+                'address_line1' => $shippingAddress->getStreet(),
+                'city' => $shippingAddress->getCity(),
+                'country_code' => $shippingAddress->getCountry()->getIso(),
+                'zip_code' => $shippingAddress->getZipCode(),
+            ],
+            'lines' => [
+                [
+                    'tax_cents' => round($order->getPrice()->getCalculatedTaxes()->getAmount() * 100),
+                    'shipping_price_cents' => round($shipping * 100),
+                    'discount_cents' => round($discount),
+                    'line_items' => $lineItems
+                ]
+            ]
+        ];
+    }
+
+    protected function getLineItems($collection, Context $context): array
+    {
+        $productIds = $collection->fmap(
+            function ($lineItem) {
+                return $lineItem->getProductId(); 
+            }
+        );
+
+        $products = $this->productRepository->search(new Criteria($productIds), $context);
+
+        $lineItems = [];
+        
+        foreach ($collection->getIterator() as $lineItem) {
+            if ($lineItem->getType() !== 'product') {
+                continue;
+            }
+
+            $product = $products->filter(function ($product) use ($lineItem) {
+                return $product->getId() == $lineItem->getProductId();
+            })->first();
+
+            if ($context->getTaxState() === CartPrice::TAX_STATE_GROSS) {
+                $unitNetPrice = ($lineItem->getPrice()->getUnitPrice() - ($lineItem->getPrice()->getCalculatedTaxes()->getAmount() / $lineItem->getQuantity())) * 100;
+            } else {
+                $unitNetPrice = $lineItem->getPrice()->getUnitPrice() * 100;
+            }
+
+            $lineItems[] = [
+                'external_reference_id' => $lineItem->getReferencedId(),
+                'product_id' => $product->getProductNumber(),
+                'quantity' => $lineItem->getQuantity(),
+                'title' => $lineItem->getLabel(),
+                'net_price_cents' => round($unitNetPrice * $lineItem->getQuantity()),
+                'net_price_per_item_cents' => round($unitNetPrice)
+            ];
+        }
+
+        return $lineItems;
+    }
+
+    protected function getDiscount($collection, Context $context): float
+    {
+        $discountAmount = 0;
+        /** @var \Shopware\Core\Checkout\Cart\LineItem\LineItem|OrderLineItemEntity $lineItem */
+        foreach ($collection->getIterator() as $lineItem) {
+            $discountLineItemType = 'discount';
+
+            if (defined( '\Shopware\Core\Checkout\Cart\LineItem\LineItem::DISCOUNT_LINE_ITEM'))
+                $discountLineItemType = \Shopware\Core\Checkout\Cart\LineItem\LineItem::DISCOUNT_LINE_ITEM;
+
+            if ($lineItem->getType() !== \Shopware\Core\Checkout\Cart\LineItem\LineItem::PROMOTION_LINE_ITEM_TYPE &&
+                $lineItem->getType() !== $discountLineItemType) {
+                continue;
+            }
+
+            if ($context->getTaxState() === CartPrice::TAX_STATE_GROSS) {
+                $unitNetPrice = ($lineItem->getPrice()->getUnitPrice() - ($lineItem->getPrice()->getCalculatedTaxes()->getAmount() / $lineItem->getQuantity())) * 100;
+            } else {
+                $unitNetPrice = $lineItem->getPrice()->getUnitPrice() * 100;
+            }
+            $discountAmount += abs($unitNetPrice);
+        }
+
+        return $discountAmount;
+    }
+
+    public function createLocalOrder($transaction, $orderUuid, $salesChannelContext) {
+        $order = $transaction->getOrder();
+        $monduOrder = $this->monduClient->setSalesChannelId($salesChannelContext->getSalesChannelId())->getMonduOrder($orderUuid);
+        
+        if (!$monduOrder) {
+            throw new AsyncPaymentProcessException($transaction->getOrderTransaction()->getId(), 'Could not fetch Mondu Order.');
+        }
 
         $this->orderDataRepository->upsert([
             [
--- src/Components/PaymentMethod/PaymentHandler/MonduSepaHandler.php
+++ src/Components/PaymentMethod/PaymentHandler/MonduSepaHandler.php
@@ -4,64 +4,7 @@ declare(strict_types=1);
 
 namespace Mondu\MonduPayment\Components\PaymentMethod\PaymentHandler;
 
-use Mondu\MonduPayment\Components\Order\Model\OrderDataEntity;
-use Mondu\MonduPayment\Components\MonduApi\Service\MonduClient;
-use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\SynchronousPaymentHandlerInterface;
-use Shopware\Core\Checkout\Payment\Cart\SyncPaymentTransactionStruct;
-use Shopware\Core\Checkout\Payment\Exception\SyncPaymentProcessException;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
-use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
-use Symfony\Component\HttpFoundation\ParameterBag;
-
-class MonduSepaHandler implements SynchronousPaymentHandlerInterface
+class MonduSepaHandler extends MonduHandler
 {
-    private OrderTransactionStateHandler $transactionStateHandler;
-    private MonduClient $monduClient;
-    private $orderDataRepository;
-    private $orderRepository;
 
-    public function __construct(OrderTransactionStateHandler $transactionStateHandler, MonduClient $monduClient, $orderDataRepository, EntityRepository $repository)
-    {
-        $this->transactionStateHandler = $transactionStateHandler;
-        $this->monduClient = $monduClient;
-        $this->orderDataRepository = $orderDataRepository;
-        $this->orderRepository = $repository;
-    }
-
-    public function pay(SyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): void
-    {
-        $monduData = $dataBag->get('mondu_payment', new ParameterBag([]));
-        if ($monduData->count() === 0 || $monduData->has('order-id') === false || !$monduData->get('order-id')) {
-            throw new SyncPaymentProcessException($transaction->getOrderTransaction()->getId(), 'unknown error during payment');
-        }
-
-        $order = $transaction->getOrder();
-        $monduOrder = $this->monduClient->setSalesChannelId($salesChannelContext->getSalesChannelId())->getMonduOrder($monduData->get('order-id'));
-        if (!$monduOrder) {
-            throw new SyncPaymentProcessException($transaction->getOrderTransaction()->getId(), 'unknown error during payment');
-        }
-
-        // Update external reference id on Mondu
-
-        $this->monduClient
-                ->setSalesChannelId($salesChannelContext->getSalesChannelId())
-                ->updateExternalInfo(
-                    $monduData->get('order-id'),
-                    ['external_reference_id' => $order->getOrderNumber()]
-                );
-
-        $this->orderDataRepository->upsert([
-            [
-                OrderDataEntity::FIELD_ORDER_ID => $order->getId(),
-                OrderDataEntity::FIELD_ORDER_VERSION_ID => $order->getVersionId(),
-                OrderDataEntity::FIELD_REFERENCE_ID => $monduOrder['uuid'],
-                OrderDataEntity::FIELD_ORDER_STATE => $monduOrder['state'],
-                OrderDataEntity::FIELD_VIBAN => $monduOrder['bank_account']['iban'],
-                OrderDataEntity::FIELD_DURATION => $monduOrder['authorized_net_term'],
-                OrderDataEntity::FIELD_IS_SUCCESSFUL => true,
-            ]
-        ], $salesChannelContext->getContext());
-    }
 }
