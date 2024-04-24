bde93089dd57dbc34eaf9966cd97aee83cfb0bb7
Tigran <62335544+tikohov20@users.noreply.github.com>
2024-04-24 14:11:14 +0400

    Hotfix for logger (#67)

--- src/Mond1SW6.php
+++ src/Mond1SW6.php
@@ -15,6 +15,15 @@ use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
 class Mond1SW6 extends Plugin
 {
     public function install(InstallContext $installContext): void
@@ -43,6 +52,24 @@ class Mond1SW6 extends Plugin
         foreach ($bootstrapper as $bootstrap) {
             $bootstrap->activate();
         }
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = \rtrim($this->getPath(), '/') . '/Resources/config';
+        $configLoader->load($confDir . '/{packages}/*.yaml', 'glob');
     }
 
     public function update(UpdateContext $updateContext): void
